6070401e7d04f6ccad76b69e6cc9b57fbde87914
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-03-06 10:27:01 -0800

    3/6

--- iphoneCalc/iphoneCalc/ContentView.swift
+++ iphoneCalc/iphoneCalc/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    let lightGray = Color(red: 211, green: 211, blue: 211)
+    let lightGray = Color(red: 211/255, green: 211/255, blue: 211/255)
     @State private var numsAndOperations: [[String]] = [
             ["AC","+/-", "%", "÷"],
             ["7","8", "9", "x"],
@@ -19,183 +19,64 @@ struct ContentView: View {
     @State private var calcScreen: Int = 0
     
     var body: some View {
+
         ZStack{
-            Color.black.edgesIgnoringSafeArea(.all)
+            Color.black.ignoresSafeArea()
             VStack{
+                Spacer()
+                
                 HStack{
                     Spacer()
                     Text("\(calcScreen)")
                         .bold()
                         .foregroundColor(.white)
                         .font(.system(size:52))
+                        .padding()
                 }
-                HStack{
-                    Button("AC", action: clear)
-                        .frame(width: 70, height: 70)
-                        .background(lightGray)
-                        .foregroundColor(.black)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("+/-", action: negNum)
-                        .frame(width: 70, height: 70)
-                        .background(lightGray)
-                        .foregroundColor(.black)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("%", action: percent)
-                        .frame(width: 70, height: 70)
-                        .background(lightGray)
-                        .foregroundColor(.black)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("÷", action: divide)
-                        .frame(width: 70, height: 70)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                }
-                HStack{
-                    Button("7", action: seven)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("8", action: eight)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("9", action: nine)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("x", action: multiply)
-                        .frame(width: 70, height: 70)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                }
-                HStack{
-                    Button("4", action: four)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("5", action: five)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("6", action: six)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("-", action: multiply)
-                        .frame(width: 70, height: 70)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                }
-                HStack{
-                    Button("1", action: four)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("2", action: five)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("3", action: six)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("+", action: multiply)
-                        .frame(width: 70, height: 70)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                }
-                HStack{
-                    Button("0", action: four)
-                        .frame(width: 150, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button(".", action: five)
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
-                    Button("=", action: six)
-                        .frame(width: 70, height: 70)
-                        .background(Color.orange)
-                        .foregroundColor(.white)
-                        .cornerRadius(35)
-                        .font(.system(size:30))
+                ForEach(numsAndOperations, id: \.self){ row in
+                    HStack{
+                        ForEach(row, id: \.self){ text in
+                            if text == "÷" || text == "x" || text == "+" || text == "-" || text == "="{
+                                Button("\(text)", action: {})
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(.orange)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            } else if text == "0"{
+                                Button("\(text)", action: {})
+                                    .font(.system(size: 32))
+                                    .frame(width: 150, height: 70)
+                                    .background(.gray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            } else if text == "AC" || text == "+/-" || text == "%"{
+                                Button("\(text)", action: {})
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(lightGray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            } else{
+                                Button("\(text)", action: {})
+                                    .font(.system(size: 32))
+                                    .frame(width: 70, height: 70)
+                                    .background(.gray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            }
+                        }
+                    }
                 }
             }
-
-            .padding()
         }
     }
-    
-    func clear(){
-        calcScreen = 0
-    }
-    func negNum(){
-        
-    }
-    func percent(){
-        
-    }
-    func divide(){
-        
-    }
-    func seven(){
-        
-    }
-    func eight(){
-        
-    }
-    func nine(){
-        
-    }
-    func four(){
-        
-    }
-    func five(){
-        
-    }
-    func six(){
-        
-    }
-    func multiply(){
-        
-    }
-    func press(){
-        
-    }
 }
+
+func handleClicks(){
+    
+}
+
 
 #Preview {
     ContentView()
